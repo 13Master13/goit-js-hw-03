e2e718b6fecef94045c8d50414ca3d8e9493502b
13Master13 <141366261+13Master13@users.noreply.github.com>
2023-11-05 22:09:47 +0200

    Settings

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,1 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-	const totalCost = quantity * pricePerDroid
 
-	if (totalCost > customerCredits) {
-		return `Insufficient funds!`
-	} else {
-		return `You ordered ${quantity} droids worth ${totalCost} credits!`
-	}
-}
